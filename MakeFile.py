4af56f9f36360c6f6b58ddbb769ee193a45526a1
Michael-C-Ebert <michaelebert757@gmail.com>
2023-03-13 01:06:52 -0500

    fully fixed the whole IOTextWrapper thing. Also tested length.py; it works

--- MakeFile.py
+++ MakeFile.py
@@ -4,7 +4,7 @@ import os
 def test_fun(filename):
     with open(filename, "x") as f:
         TextWrapper = str(f)
-        cut_one = TextWrapper.index("name=")
+        cut_one = TextWrapper.index("name=")+5
         cut_two = TextWrapper.index(" mode=")
         MessagePiece = TextWrapper[cut_one:cut_two]
         print("The file known as {} has been born!".format(MessagePiece))
