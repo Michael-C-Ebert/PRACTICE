cc5f959fd6873d602fa839430bc7702193ae8165
Michael-C-Ebert <michaelebert757@gmail.com>
2023-03-13 01:03:27 -0500

    partially fixeed the IOTextWrapper problem; now it just has the 'name=' part

--- MakeFile.py
+++ MakeFile.py
@@ -3,6 +3,10 @@ import os
 
 def test_fun(filename):
     with open(filename, "x") as f:
-        print("The file known as {} has been born!".format(str(f)))
+        TextWrapper = str(f)
+        cut_one = TextWrapper.index("name=")
+        cut_two = TextWrapper.index(" mode=")
+        MessagePiece = TextWrapper[cut_one:cut_two]
+        print("The file known as {} has been born!".format(MessagePiece))
 
 test_fun("/home/mikeyebert2004/testdir/testfile.txt")
