2eb97b4a118f8fa6a5efa089c5bc872906d85fd3
Michael-C-Ebert <michaelebert757@gmail.com>
2023-03-13 00:50:37 -0500

    Got MakeFile.py to make the file; however, the message saying the file's made say 'io.TextIOWrapper'. Also removed the try/except block

--- MakeFile.py
+++ MakeFile.py
@@ -5,7 +5,4 @@ def test_fun(filename):
     with open(filename, "x") as f:
         print("The file known as {} has been born!".format(str(f)))
 
-try:
-    test_fun("~/home/mikeyebert2004/testdir/testfile.txt")
-except:
-    print(os.system('pwd'))
+test_fun("/home/mikeyebert2004/testdir/testfile.txt")
