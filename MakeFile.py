a7ef5b985683e86fbe5b7f6526d4af701113936c
Michael-C-Ebert <michaelebert757@gmail.com>
2023-03-14 23:20:13 -0500

    made it so MakeFile.py will print 'File already exists' instead of resulting in an error message if it's run to make a file that already exists

--- MakeFile.py
+++ MakeFile.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import os
+from pathlib import Path
 
 def test_fun(filename):
-    with open(filename, "x") as f:
-        TextWrapper = str(f)
-        cut_one = TextWrapper.index("name=")+5
-        cut_two = TextWrapper.index(" mode=")
-        MessagePiece = TextWrapper[cut_one:cut_two]
-        print("The file known as {} has been born!".format(MessagePiece))
+    path = Path(filename)
+    if path.is_file() == False:
+        with open(filename, "x") as f:
+            TextWrapper = str(f)
+            cut_one = TextWrapper.index("name=")+5
+            cut_two = TextWrapper.index(" mode=")
+            MessagePiece = TextWrapper[cut_one:cut_two]
+            print("The file known as {} has been born!".format(MessagePiece))
+    else:
+        print("File already exists")
 
 test_fun("/home/mikeyebert2004/testdir/testfile.txt")
