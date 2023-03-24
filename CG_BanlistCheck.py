6a64fb337cfa174d1676115c1d72abf1b55d2709
Michael-C-Ebert <michaelebert757@gmail.com>
2023-03-24 13:41:02 -0500

    got the non-card remover in CG_BanlistCheck.py made and working fine

--- CG_BanlistCheck.py
+++ CG_BanlistCheck.py
@@ -3,7 +3,11 @@
 decklist = input("Which decklist would you like to scan?: ")
 
 with open(decklist) as deck:
-   cards = deck.readlines()
-   for line in cards:
-       if line.startswith('//'):
-           cards -= line
+    cards = deck.readlines()
+    for line in cards:
+        if line.startswith('//'):
+            cards.remove(line)
+    if 'deck-1' in cards:
+        print("Non-card remover needs work")
+    else:
+        print("Non-card remover seems to work fine")
