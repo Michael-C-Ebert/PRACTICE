4006a4431a65d51e83ae2e3a68da4ba71fd2c055
Michael-C-Ebert <michaelebert757@gmail.com>
2023-04-25 16:06:01 -0500

    just tested timezone.py; right now, it only shows the timezoone and not the time

--- timezone.py
+++ timezone.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
 CST_hour = int(input("Enter the CST hour here: "))
-CST_minutes = int((input("Enter the CST minutes here: "))
+CST_minutes = int(input("Enter the CST minutes here: "))
 timezones = ["CST", "CET", "BST", "UTC+3"]
-time_stamps = {}
+time_stamps = {"timezone":"time"}
+minutes = ":" + str(CST_minutes)
 
-CST_time = str(CST_hour) + ":" + str(CST_minutes)
-print(CST_time)
+CST_time = str(CST_hour) + minutes
+time_stamps["CST time"] = CST_time
+
+CET_time = str(CST_hour+7) + minutes
+time_stamps["CET time"] = CET_time
+
+BST_time = str(CST_hour+6) + minutes
+time_stamps["BST time"] = BST_time
+
+UTC3_time = str(CST_hour+8) + minutes
+time_stamps["UTC+3 time"] = UTC3_time
+
+for item in time_stamps:
+    print(item)
