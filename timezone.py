cb4962fb4012bdcde31c8b47d02cc542fdce01d9
Michael-C-Ebert <michaelebert757@gmail.com>
2023-04-25 15:50:38 -0500

    created timezone.py

---
+++ timezone.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+CST_hour = int(input("Enter the CST hour here: "))
+CST_minutes = int((input("Enter the CST minutes here: "))
+timezones = ["CST", "CET", "BST", "UTC+3"]
+time_stamps = {}
+
+CST_time = str(CST_hour) + ":" + str(CST_minutes)
+print(CST_time)
