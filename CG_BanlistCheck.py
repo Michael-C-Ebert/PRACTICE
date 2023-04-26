ec31460b8e495ff4c0456ab49ab409eaf1509ee2
Michael-C-Ebert <michaelebert757@gmail.com>
2023-04-25 20:48:28 -0500

    CG_BanlistCheck.py now has a dictionary with the card names as keys and the card quantities as values

--- CG_BanlistCheck.py
+++ CG_BanlistCheck.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 decklist = input("Which decklist would you like to scan?: ")
+card_dict = {}
 
 with open(decklist) as deck:
     cards = deck.readlines()
     for line in cards:
+        new_line_index = line.index("\n")
         if line.startswith('//'):
             cards.remove(line)
-    if 'deck-1' in cards:
-        print("Non-card remover needs work")
-    else:
-        print("Non-card remover seems to work fine")
+        cardname = line[2:new_line_index]
+        quantity = line[0]
+        card_dict[cardname] = quantity
+
+print(card_dict)
